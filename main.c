4750c7a5b03f0c3cee52f2eac99943420bb99d16
sunke <cx.sunke@gmail.com>
2013-08-31 13:53:22 -0700

    在main中添加第二个fifo

--- main.c
+++ main.c
@@ -36,15 +36,43 @@ fifo_read(EventSelector *es,
 	}
 
 	buf[len] = '\0';
-	fprintf(stdout, "Read: %s\n", buf);
+    printf("func:%s,line:%d, %s\n",__func__,__LINE__, buf);
 }
+static void
+fifo2_read(EventSelector *es,
+		 int fd,
+		 unsigned int flags,
+		 void *data)
+{
+	char buf[255];
+	int len;
+
+    memset(buf,0,sizeof(buf));
+	len = read(fd, buf, sizeof(buf) - 1);
+	if (len == -1) 
+    {
+		perror("read");
+		return;
+	} 
+    else if (len == 0) 
+    {
+		fprintf(stderr, "Connection closed\n");
+		return;
+	}
+
+	buf[len] = '\0';
+    printf("func:%s,line:%d, %s\n",__func__,__LINE__, buf);
+}
+
 
 
 int main ( int argc, char *argv[] )
 {
 	struct stat st;
 	const char *fifo = "event.fifo";
+	const char *fifo2 = "event.fifo2";
 	int socket;
+	int socket2;
  
 	if (lstat (fifo, &st) == 0)
 	{
@@ -55,15 +83,36 @@ int main ( int argc, char *argv[] )
 			exit (1);
 		}
 	}
+	if (lstat (fifo2, &st) == 0)
+	{
+		if ((st.st_mode & S_IFMT) == S_IFREG)
+        {
+			errno = EEXIST;
+			perror("lstat");
+			exit (1);
+		}
+	}
 
 	unlink (fifo);
+	unlink (fifo2);
 	if (mkfifo (fifo, 0600) == -1)
+	{
+		perror("mkfifo");
+		exit (1);
+	}
+	if (mkfifo (fifo2, 0600) == -1)
 	{
 		perror("mkfifo");
 		exit (1);
 	}
 
 	socket = open (fifo, O_RDWR | O_NONBLOCK, 0);
+	if (socket == -1) 
+	{
+		perror("open");
+		exit (1);
+	}
+	socket2 = open (fifo2, O_RDWR | O_NONBLOCK, 0);
 	if (socket == -1) 
 	{
 		perror("open");
@@ -81,6 +130,11 @@ int main ( int argc, char *argv[] )
 					    EVENT_FLAG_READABLE,
 					    fifo_read,
 					    &socket);
+	EventHandler *event_handle2 = Event_AddHandler(event_selector,
+					    socket2,
+					    EVENT_FLAG_READABLE,
+					    fifo2_read,
+					    &socket2);
 
     for(;;)
     {
